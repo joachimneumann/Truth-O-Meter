be17ed024d47cbfe6601777723e930c3fc5fadf0
Joachim Neumann <joachim@mindo.io>
2021-08-20 18:14:38 +0700

    animating the needle

---
+++ Shared/Test.swift
@@ -0,0 +1,20 @@
+//
+//  Test.swift
+//  Truth-O-Meter
+//
+//  Created by Joachim Neumann on 20/08/2021.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Test_Previews: PreviewProvider {
+    static var previews: some View {
+        Test()
+    }
+}
