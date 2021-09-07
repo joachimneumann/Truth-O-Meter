dce4d837176523e6707b6da2d45784610811343c
Joachim Neumann <joachim@mindo.io>
2021-09-07 22:28:13 +0700

    improvements on stamp

--- Shared/Views/StampView.swift
+++ Shared/Views/StampView.swift
@@ -17,58 +17,125 @@ struct StampView: View {
     var body: some View {
         Playground(text: top)
     }
-}
-
-struct AutosizeText: View {
-    var text: String
-    var textColor: Color
-    var frameWidth: CGFloat
-    var frameHeight: CGFloat
-    @Binding var textSize: CGSize
-    @Binding var border: CGFloat
-
-    var body: some View {
-        let scalew = frameWidth / (textSize.width)
-        let scaleh = frameHeight / (textSize.height)
-        let scale = min(scalew, scaleh)
-        return Text(text)
-            .font(.system(size: 300))  // Bigger font size then final rendering
-            .foregroundColor(textColor)
-            .fixedSize() // Prevents text truncating
-            .captureSize(in: $textSize)
-            .background(Color.black.opacity(0.05))
-            .scaleEffect(scale)
-            .onChange(of: frameHeight, perform: { value in
-                let verticalTextMargin = textSize.height / 16
-                let scalew = frameWidth / (textSize.width+2*verticalTextMargin)
-                let scaleh = frameHeight / (textSize.height+2*verticalTextMargin)
-                let scale = min(scalew, scaleh)
-                border = verticalTextMargin * scale
-            })
-            .onChange(of: frameWidth, perform: { value in
-                let verticalTextMargin = textSize.height / 16
-                let scalew = frameWidth / (textSize.width+2*verticalTextMargin)
-                let scaleh = frameHeight / (textSize.height+2*verticalTextMargin)
-                let scale = min(scalew, scaleh)
-                border = verticalTextMargin * scale
-            })
+    /*
+     struct AutosizeText: View {
+     var text: String
+     var textColor: Color
+     var frameWidth: CGFloat
+     var frameHeight: CGFloat
+     @Binding var textSize: CGSize
+     @Binding var border: CGFloat
+     
+     var body: some View {
+     let scalew = frameWidth / (textSize.width)
+     let scaleh = frameHeight / (textSize.height)
+     let scale = min(scalew, scaleh)
+     return Text(text)
+     .font(.system(size: 300))  // Bigger font size then final rendering
+     .foregroundColor(textColor)
+     .fixedSize() // Prevents text truncating
+     .captureSize(in: $textSize)
+     .background(Color.black.opacity(0.05))
+     .scaleEffect(scale)
+     .onChange(of: frameHeight, perform: { value in
+     let verticalTextMargin = textSize.height / 16
+     let scalew = frameWidth / (textSize.width+2*verticalTextMargin)
+     let scaleh = frameHeight / (textSize.height+2*verticalTextMargin)
+     let scale = min(scalew, scaleh)
+     border = verticalTextMargin * scale
+     })
+     .onChange(of: frameWidth, perform: { value in
+     let verticalTextMargin = textSize.height / 16
+     let scalew = frameWidth / (textSize.width+2*verticalTextMargin)
+     let scaleh = frameHeight / (textSize.height+2*verticalTextMargin)
+     let scale = min(scalew, scaleh)
+     border = verticalTextMargin * scale
+     })
+     }
+     }
+     */
+    
+    struct StampTextWithBorder: View {
+        var text: String
+        var color: Color
+        var margin: CGFloat
+        var borderWidth: CGFloat
+        var body: some View {
+            Text(text)
+                .font(.system(size: 300))
+                .fixedSize() // Prevents text truncating
+                .background(Color.black.opacity(0.2))
+                .padding(margin)
+                .background(Color.red.opacity(0.6))
+                .padding(borderWidth)
+                .border(Color.green.opacity(0.2), width: borderWidth)
+        }
     }
-}
+    
+    struct SmartStampText: View {
+        @State private var textSize = CGSize(width: 100, height: 100)
 
-struct StampText: View {
-    var text: String
-    var color: Color
-    var frameWidth: CGFloat
-    var frameHeight: CGFloat
-    @State private var textSize = CGSize(width: 200, height: 1000)
-    @State private var border: CGFloat = 15.0
-
-    var body: some View {
-        AutosizeText(text: text, textColor: color, frameWidth: frameWidth, frameHeight: frameHeight, textSize: $textSize, border: $border)
-            .frame(width: frameWidth, height: frameHeight)
-            .padding(border)
-            .border(color)
-            .background(Color.yellow.opacity(0.3))
+        var text: String
+        var color: Color
+        var frameWidth: CGFloat
+        var frameHeight: CGFloat
+        private let marginFactor: CGFloat = 0.05
+        private let borderWidthFactor: CGFloat = 0.05
+        
+        var body: some View {
+            let marginWidth: CGFloat = textSize.height * marginFactor
+            let borderWidth: CGFloat = textSize.height * borderWidthFactor
+            let scale = frameHeight/(textSize.height+marginWidth+borderWidth)
+            ZStack {
+                // invisible Text --> textSize
+                Text(text)
+                    .foregroundColor(Color.clear)
+                    .font(.system(size: 300))  // Bigger font size then final rendering
+                    .fixedSize() // Prevents text truncating
+                    .background(
+                        GeometryReader { (geo) -> Color in
+                            DispatchQueue.main.async {  // hack for modifying state during view rendering.
+                                textSize = geo.size
+                            }
+                            return Color.clear
+                        }
+                    )
+                StampTextWithBorder(
+                    text: text,
+                    color: color,
+                    margin: marginWidth,
+                    borderWidth: borderWidth)
+                    .scaleEffect(scale, anchor: .center)
+            }
+            //            .captureSize(in: $textSize)
+        }
+    }
+//    struct StampText: View {
+//        var text: String
+//        var color: Color
+//        var frameWidth: CGFloat
+//        var frameHeight: CGFloat
+//        @State private var textSize = CGSize(width: 200, height: 1000)
+//        @State private var border: CGFloat = 15.0
+//
+//        var body: some View {
+//            AutosizeText(text: text, textColor: color, frameWidth: frameWidth, frameHeight: frameHeight, textSize: $textSize, border: $border)
+//                .frame(width: frameWidth, height: frameHeight)
+//                .padding(border)
+//                .border(color)
+//                .background(Color.yellow.opacity(0.3))
+//        }
+//    }
+    struct Playground: View {
+        var text: String
+        @State private var frameWidth: CGFloat = 175
+        @State private var frameHeight: CGFloat = 175
+        
+        var body: some View {
+            FrameAdjustingContainer(frameWidth: $frameWidth, frameHeight: $frameHeight) {
+                SmartStampText(text: text, color: C.color.bullshitRed, frameWidth: frameWidth, frameHeight: frameHeight)
+            }
+        }
     }
 }
 
@@ -78,6 +145,7 @@ private struct SizeKey: PreferenceKey {
         value = nextValue()
     }
 }
+
 
 extension View {
     func captureSize(in binding: Binding<CGSize>) -> some View {
@@ -114,21 +182,9 @@ struct FrameAdjustingContainer<Content: View>: View {
     }
 }
 
-struct Playground: View {
-    var text: String
-    @State private var frameWidth: CGFloat = 175
-    @State private var frameHeight: CGFloat = 175
-    
-    var body: some View {
-        FrameAdjustingContainer(frameWidth: $frameWidth, frameHeight: $frameHeight) {
-            StampText(text: text, color: C.color.bullshitRed, frameWidth: frameWidth, frameHeight: frameHeight)
-        }
-    }
-}
-
 
 struct Stamp_Previews: PreviewProvider {
     static var previews: some View {
-        StampView(top: "1Éj", bottom: "33", rotated: false, color: Color.blue)
+        StampView(top: "1Éj", bottom: "33", rotated: true, color: Color.blue)
     }
 }
