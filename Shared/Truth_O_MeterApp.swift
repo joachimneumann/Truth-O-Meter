4815700268ebaa4df62e4209f53a5c4620ef809f
Joachim Neumann <joachim@mindo.io>
2021-09-11 21:55:30 +0700

    Catalyst: fixed window size

--- Shared/Truth_O_MeterApp.swift
+++ Shared/Truth_O_MeterApp.swift
@@ -10,16 +10,15 @@ import NavigationStack
 
 @main
 struct Truth_O_MeterApp: App {
+    #if targetEnvironment(macCatalyst)
+    @UIApplicationDelegateAdaptor var delegate: FSAppDelegate
+    #endif
+
     var body: some Scene {
         //let settings = Settings()
         Needle.shared.active(true, strongNoise: false)
         return WindowGroup {
-            #if targetEnvironment(macCatalyst)
             PlaygroundView()
-                .frame(width: 375, height: 667)
-            #else
-            PlaygroundView()
-            #endif
             //                .background(Color.white)
             
             
@@ -30,3 +29,32 @@ struct Truth_O_MeterApp: App {
         }
     }
 }
+
+#if targetEnvironment(macCatalyst)
+class FSSceneDelegate: NSObject, UIWindowSceneDelegate, ObservableObject {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        UIApplication.shared.connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .forEach { windowScene in
+                windowScene.sizeRestrictions?.minimumSize = CGSize(width: 375*1.5, height: 667*1.5)
+                windowScene.sizeRestrictions?.maximumSize = CGSize(width: 375*1.5, height: 667*1.5)
+            }
+    }
+}
+
+class FSAppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let sceneConfig = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        sceneConfig.delegateClass = FSSceneDelegate.self // 👈🏻
+        return sceneConfig
+    }
+}
+#endif
