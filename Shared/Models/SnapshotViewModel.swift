a40665d9b13f30e5eaddd9152067ef99a3c4953f
Joachim Neumann <joachim@mindo.io>
2021-09-11 11:39:44 +0700

    snap()

---
+++ Shared/Models/SnapshotViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SnapshotViewModel.swift
+//  Truth-O-Meter
+//
+//  Created by Joachim Neumann on 11/09/2021.
+//
+
+import Foundation
--- Shared/Views/SnapshotView.swift
+++ Shared/Views/SnapshotView.swift
@@ -5,73 +5,67 @@
 //  Created by Joachim Neumann on 11/09/2021.
 //
 
-//
-//  Stamp.swift
-//  Truth-O-Meter
-//
-//  Created by Joachim Neumann on 08/09/2021.
-//
-
 import SwiftUI
 
-// TODO: do not use global variables
-var snapshotImage: UIImage? = nil
-
 struct SnapshotView: View {
+    @StateObject var snapshotViewModel = SnapshotViewModel()
     var text: String
     var color: Color
     let fontSize:CGFloat
     var angle: Angle
     
-    var snapshotTaken = false
-
     @State var size: CGSize = CGSize(width: 100, height: 100)
 
     var Snapshot: some View {
-        Stamp(
-            text: text,
-            color: color,
-            fontSize: fontSize,
-            angle: angle)
-//        let margin      = fontSize * 0.4
-//        let borderWidth = fontSize * 0.4
-//        let stampPadding = StampPadding(size, angle: angle)
-//        return Text(text)
-//            .foregroundColor(color)
-//            .font(.system(size: fontSize))
-//            .lineLimit(1)
-//            .padding(margin)
-//            .padding(borderWidth/2)
-//            .overlay(
-//                RoundedRectangle(
-//                    cornerRadius: borderWidth*1.5)
-//                    .stroke(color, lineWidth: borderWidth))
-//            .padding(borderWidth/2)
-//            .background(Color.green.opacity(0.2))
-//            .stampCaptureSize(in: $size)
-//            .rotationEffect(angle)
-//            .padding(.leading,  stampPadding.horizontal)
-//            .padding(.trailing, stampPadding.horizontal)
-//            .padding(.top,      stampPadding.vertical)
-//            .padding(.bottom,   stampPadding.vertical)
-//            .background(Color.green.opacity(0.2))
+//        Stamp(
+//            text: text,
+//            color: color,
+//            fontSize: fontSize,
+//            angle: angle)
+        let margin      = fontSize * 0.4
+        let borderWidth = fontSize * 0.4
+        let stampPadding = StampPadding(size, angle: angle)
+        return Text(text)
+            .foregroundColor(color)
+            .font(.system(size: fontSize))
+            .lineLimit(1)
+            .padding(margin)
+            .padding(borderWidth/2)
+            .overlay(
+                RoundedRectangle(
+                    cornerRadius: borderWidth*1.5)
+                    .stroke(color, lineWidth: borderWidth))
+            .padding(borderWidth/2)
+            .background(Color.green.opacity(0.2))
+            .stampCaptureSize(in: $size)
+            .rotationEffect(angle)
+            .padding(.leading,  stampPadding.horizontal)
+            .padding(.trailing, stampPadding.horizontal)
+            .padding(.top,      stampPadding.vertical)
+            .padding(.bottom,   stampPadding.vertical)
+            .background(Color.green.opacity(0.2))
     }
 
     var body: some View {
         VStack {
-            if let s = snapshotImage {
+            if false {//snapshotViewModel.snapshotTaken {
                 VStack{
                     Text("snapshot taken!")
-                    Image(uiImage: s)
+                    Image(uiImage: snapshotViewModel.snapshot!)
+                        .resizable()
+                        .scaledToFit()
                 }
             } else {
-                Snapshot
+                VStack{
+                    Text("working...")
+                    Snapshot
+                }
             }
         }
         .onAppear() {
             DispatchQueue.main.async {
-                snapshotImage = Snapshot.snapshot()
-                if let s = snapshotImage {
+                snapshotViewModel.snap(image: Snapshot.snapshot())
+                if let s = snapshotViewModel.snapshot {
                     UIImageWriteToSavedPhotosAlbum(s, nil, nil, nil)
                 }
             }
