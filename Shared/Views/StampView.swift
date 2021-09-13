df7d11c3cf1c211f25229153d44489596921bd2e
Joachim Neumann <joachim@mindo.io>
2021-09-13 18:32:55 +0700

    works with rotation

--- Shared/Views/StampView.swift
+++ Shared/Views/StampView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct StampView: View {
     let text: String
-    let largeFontSize = 300.0
+    let largeFontSize = 100.0
     @State var frameSize: CGSize = CGSize(width: 100, height: 100)
     @State var textSize: CGSize = CGSize(width: 100, height: 100)
     var body: some View {
@@ -21,30 +21,22 @@ struct StampView: View {
             VStack {
                 Text("frame (\(frameSize.width.s), \(frameSize.height.s))\ntext  (\(textSize.width.s), \(textSize.height.s)),\nscale \(scaleHorizontal)")
                     .font(.system(size: 14, design: .monospaced))
-                //            HStack {
-    //                Text("This is a short string.")
-    //                    .padding()
-    //                    .frame(maxHeight: .infinity)
-    //                    .background(Color.red)
-    //
-    //                Text("This is a very long string with lots and lots of text that will definitely run across")
-    //                    .padding()
-    //                    .frame(maxHeight: .infinity)
-    //                    .background(Color.green)
-    //            }
-    //            .fixedSize(horizontal: false, vertical: true)
-    //            .frame(maxHeight: 150)
                 VStack {
                     Rectangle()
                         .foregroundColor(.green.opacity(0.2))
                         .frame(height: 310)
                         .background(
-                            Text("iiiiiiiii")
+//                            Text("iiiiiiiii")
+                            Text(text)
                                 .font(.system(size: largeFontSize))
                                 .minimumScaleFactor(0.01)
+                                .lineLimit(1)
                                 .captureSize(in: $textSize)
                                 .background(Color.pink.opacity(0.2))
-                                .scaleEffect(scaleHorizontal)
+                                .padding(textSize.width*0.05)
+                                .border(Color.green, width: 3)
+                                .rotationEffect(Angle(degrees: -25))
+                                .scaleEffect(scaleHorizontal*0.89)
                         )
                     Rectangle()
                         .foregroundColor(.yellow.opacity(0.2))
@@ -54,37 +46,8 @@ struct StampView: View {
             }
         }
         .padding()
+    }
 
-        
-        //
-        //        let scaleHorizontal = frameSize.width / textSize.width
-        ////        let scaleHorizontal = textSize.width / frameSize.width
-        //        ZStack {
-        //            Group {
-        //            CaptureText(text: text, fontSize: largeFontSize, textSize: $textSize)
-        //            CaptureFrame(frameSize: $frameSize)
-        //            }
-        //            VStack {
-        //                HorizontalStamp(
-        //                    text: text,
-        //                    fontSize: largeFontSize)
-        //                    .background(Color.green.opacity(0.2))
-        //                    .scaleEffect(scaleHorizontal)
-        //                Text("frame (\(frameSize.width.s), \(frameSize.height.s))\ntext  (\(textSize.width.s), \(textSize.height.s)),\nscale \(scaleHorizontal)")
-        //                    .font(.system(size: 14, design: .monospaced))
-        //                    .padding()
-    }
-    
-    struct HorizontalStamp: View {
-        let text: String
-        let fontSize: Double
-        var body: some View {
-            Text(text)
-                .lineLimit(1)
-                .fixedSize()
-                .font(.system(size: fontSize))
-        }
-    }
     struct CaptureFrame: View {
         @Binding var frameSize: CGSize
         var body: some View {
