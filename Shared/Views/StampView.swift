94a4fdad80a65787201470c862a23e083a7ec005
Joachim Neumann <joachim@mindo.io>
2021-09-14 10:34:56 +0700

    refactoring

--- Shared/Views/StampView.swift
+++ Shared/Views/StampView.swift
@@ -10,64 +10,78 @@ import SwiftUI
 var textCaptured = false
 var frameCaptured = false
 
+struct FrameCatcher: View {
+    @Binding var into: CGSize
+    var body: some View {
+        Rectangle()
+            .foregroundColor(.clear)//.blue.opacity(0.2))
+            .background(
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .captureSize(in: $into)
+            )
+    }
+}
+
+struct Calc {
+    let padding: Double
+    let borderwidth: Double
+    let scale: Double
+    init(frameSize: CGSize, textSize: CGSize) {
+        let marginFactor      = 0.2
+        let borderwidthFactor = 0.2
+
+        let marginAndBorderFactor = marginFactor + borderwidthFactor
+        padding = textSize.height * marginAndBorderFactor
+        borderwidth = textSize.height * borderwidthFactor
+        
+        let scaleHorizontal = frameSize.width / textSize.width
+        let marginCorrection = textSize.width / (textSize.width + 2.0 * marginAndBorderFactor * textSize.height)
+        scale = scaleHorizontal * marginCorrection
+    }
+}
+
 struct StampView: View {
     let top: String
     let largeFontSize = 100.0
     @State var frameSize = CGSize(width: 1.0, height: 1.0)
     @State var textSize  = CGSize(width: 1.0, height: 1.0)
-
+    
     let debugInfo = false
     var body: some View {
+        
+        let calc = Calc(frameSize: frameSize, textSize: textSize)
 
-//        let scale = frameSize.width / textSize.width
-        let scaleHorizontal = frameSize.width / textSize.width
-//        let borderwidth = 0.5
-        let marginX = 0.4//borderwidth + 0.25
-        let marginFactor = textSize.width / (textSize.width + 2.0 * marginX * textSize.height)
-        let scale = scaleHorizontal * marginFactor
         ZStack {
-            if true{//}!frameCaptured {
-                Rectangle()
-                    .foregroundColor(.clear)//.blue.opacity(0.2))
-                    .background(
-                        Rectangle()
-                            .foregroundColor(.clear)
-                            .captureSize(in: $frameSize)
-                    )
-            }
-            //let _ = print("StampView VStack scaleHorizontal = \(scaleHorizontal.s) frameSize = \(frameSize) textSize = \(textSize)")
+            FrameCatcher(into: $frameSize)
             let _ = print("frameSize = \(frameSize) textSize = \(textSize)")
-            let _ = print("scaleHorizontal = \(scaleHorizontal) marginFactor = \(marginFactor) scale \(scale)")
-            if true { //}!frameCaptured || !textCaptured {
-                VStack {
-                    let _ = print("StampView VStack")
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(.clear)//green.opacity(0.2))
-                            .background(
-                                Text(top)
-                                    .font(.system(size: largeFontSize))
-                                    .fixedSize()
-                                    .lineLimit(1)
-                                    .captureSize(in: $textSize)
-                                    .background(Color.red.opacity(0.2))
-                                    .padding(textSize.height*0.4)
-                                    .background(Color.red.opacity(0.2))
-//                                    .border(Color.green, width: borderwidth)
-                                    //.animation(.linear(duration: 0.0))
-//                                    .background(Color.green.opacity(0.2))
-                            )
-                    }
-                    .fixedSize(horizontal: true, vertical: true)
+            let _ = print("scale \(calc.scale)")
+            VStack {
+                let _ = print("StampView VStack")
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.clear)//green.opacity(0.2))
+                        .background(
+                            Text(top)
+                                .font(.system(size: largeFontSize))
+                                .fixedSize()
+                                .lineLimit(1)
+                                .captureSize(in: $textSize)
+                                .background(Color.red.opacity(0.2))
+                                .padding(calc.padding)
+                                .background(Color.red.opacity(0.2))
+                                .border(Color.green, width: calc.borderwidth)
+                        )
                 }
-                .scaleEffect(scale)
+                .fixedSize(horizontal: true, vertical: true)
             }
+            .scaleEffect(calc.scale)
         }
     }
 }
 
 struct StampView_Previews: PreviewProvider {
     static var previews: some View {
-        StampView(top: "Bullshit")
+        StampView(top: "iiiiiiii")
     }
 }
