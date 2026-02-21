b1303804ad40db4f0dc7e506cac9b0d6a80da5d0
Joachim Neumann <joachim@joachimneumann.com>
2026-02-21 22:17:22 +0700

    small improvements to horizontal progress bar

--- Shared/Views/HorizontalProgressBar.swift
+++ Shared/Views/HorizontalProgressBar.swift
@@ -5,39 +5,47 @@
 //  Created by Joachim Neumann on 20/08/2021.
 //
 
-import Foundation
 import SwiftUI
 
 struct HorizontalProgressBar: View {
     let activeColor: Color
     let passiveColor: Color
     let animationTime: Double
-    let height:CGFloat = 4
-    
-    @State var animate: Bool = false
+
+    private let barHeight: CGFloat = 4
+
+    @State private var isAnimating = false
 
     var body: some View {
-        ZStack(alignment: Alignment.leading) {
-            Rectangle()
-                .foregroundColor(passiveColor)
-                .frame(height: height)
-                .opacity(0.2)
-            Rectangle()
-                .foregroundColor(activeColor)
-                .frame(width: animate ? .infinity : 0, height: height)
-                .animation(.easeIn(duration: animationTime), value: animate)
-                .onAppear() {
-                    animate = true
-                }
+        ZStack(alignment: .leading) {
+            backgroundBar
+            foregroundBar
         }
+    }
+
+    private var backgroundBar: some View {
+        Rectangle()
+            .foregroundColor(passiveColor)
+            .frame(height: barHeight)
+            .opacity(0.2)
+    }
+
+    private var foregroundBar: some View {
+        Rectangle()
+            .foregroundColor(activeColor)
+            .frame(height: barHeight)
+            .scaleEffect(x: isAnimating ? 1 : 0, y: 1, anchor: .leading)
+            .animation(.easeIn(duration: animationTime), value: isAnimating)
+            .onAppear {
+                isAnimating = true
+            }
     }
 }
 
 struct HorizontalProgressbar_Previews: PreviewProvider {
     static var previews: some View {
-        func doNothing() {}
-        return HorizontalProgressBar(activeColor: Color.red,
-                                     passiveColor: Color.gray,
-                                     animationTime: 2)
+        HorizontalProgressBar(activeColor: .red,
+                              passiveColor: .gray,
+                              animationTime: 2)
     }
 }
